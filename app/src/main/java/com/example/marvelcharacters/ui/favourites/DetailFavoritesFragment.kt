d02f9819c4577d5faa103d3a333acb85dc2294ad
Juan Fran <jmartifr@everis.com>
2021-11-18 08:13:28 +0100

    Show comics

--- app/src/main/java/com/example/marvelcharacters/ui/favourites/DetailFavoritesFragment.kt
+++ app/src/main/java/com/example/marvelcharacters/ui/favourites/DetailFavoritesFragment.kt
@@ -1,5 +1,7 @@
 package com.example.marvelcharacters.ui.favourites
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +14,7 @@ import com.bumptech.glide.Glide
 import com.example.marvelcharacters.R
 import com.example.marvelcharacters.data.local.models.CharactersEntity
 import com.example.marvelcharacters.databinding.FragmentDetailBinding
+import com.example.marvelcharacters.ui.ComicsAdapter
 import com.example.marvelcharacters.ui.detail.DetailFragmentArgs
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,6 +25,7 @@ class DetailFavoritesFragment : Fragment() {
     lateinit var binding: FragmentDetailBinding
     private val detailFavoritesViewModel: DetailFavoritesViewModel by viewModels()
     private val args: DetailFragmentArgs by navArgs()
+    lateinit var adapter: ComicsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,14 +37,20 @@ class DetailFavoritesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        detailFavoritesViewModel.getCharacter(args.characterId).observe(viewLifecycleOwner) { characters ->
-            if(characters != null){
-                bindingData(characters)
-            }else{
-                Snackbar.make(binding.root, getString(R.string.error_ocurred), Snackbar.LENGTH_LONG)
-                    .show()
+        detailFavoritesViewModel.getCharacter(args.characterId)
+            .observe(viewLifecycleOwner) { characters ->
+                if (characters != null) {
+                    bindingData(characters)
+                } else {
+                    Snackbar.make(
+                        binding.root,
+                        getString(R.string.error_ocurred),
+                        Snackbar.LENGTH_LONG
+                    )
+                        .show()
+                }
             }
-        }
+        initComicAdapter()
         initListeners()
     }
 
@@ -50,15 +60,31 @@ class DetailFavoritesFragment : Fragment() {
         }
     }
 
+    fun initComicAdapter() {
+        adapter = ComicsAdapter(
+            object : ComicsAdapter.ListItemComicClickListener {
+                override fun onClickItem(nameComic: String) {
+                    val browserIntent = Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://www.google.com/search?q=$nameComic")
+                    )
+                    startActivity(browserIntent)
+                }
+            }
+        )
+        binding.comicsList.adapter = adapter
+    }
+
     fun bindingData(characters: CharactersEntity) {
         detailFavoritesViewModel.getCharacter(args.characterId)
         binding.apply {
             tvName.text = characters.name
             loadImage(characters.thumbnail_path)
-            tvDescription.text = if(characters.description != "") characters.description else getString(R.string.character_without_description)
-            tvModified.text = getString(R.string.updated_on)+" "+ characters.modified
+            tvDescription.text =
+                if (characters.description != "") characters.description else getString(R.string.character_without_description)
+            tvModified.text = getString(R.string.updated_on) + " " + characters.modified
+            (comicsList.adapter as ComicsAdapter).submitList(characters.comics)
         }
-
     }
 
     private fun loadImage(thumbnailPath: String) {

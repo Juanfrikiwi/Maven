0b11df241ce5ffd3ed000d3491d572da05170666
Juan Fran <jmartifr@everis.com>
2021-12-01 17:30:58 +0100

    .

--- app/src/main/java/com/example/marvelcharacters/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/marvelcharacters/ui/home/HomeFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SearchView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.LoadState
